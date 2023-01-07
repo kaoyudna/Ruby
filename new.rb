a16886f7b4f5630a25e3ef78ddbed17ac694b8f3
EC2 Default User <ec2-user@ip-172-31-3-94.ap-northeast-1.compute.internal>
2023-01-07 01:42:24 +0000

    [Finish]2-9 new.rb

--- new.rb
+++ new.rb
@@ -1,12 +1,29 @@
 class Car
-  def turn(houkou)
-    puts "#{houkou}へ曲がります。"
-  end 
+
+  def move(direction,distance)
+    self.turn(direction)
+    self.run (distance)
+  end
+
+  def turn(direction)
+    puts "#{direction}へ曲がります。"
+  end
+
   def run(distance)
     puts "車で#{distance}キロ走ります。"
   end
-end 
+end
 
 car = Car.new
-car.turn("右")
-car.run(5)
+car.move("右",5)
+
+
+
+
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
